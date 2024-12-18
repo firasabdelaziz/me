1d7bad7f784850653b48fc8098b6fd2bf243787d
Firas_Abdelaziz <firasabdelaziz123@gmail.com>
2024-12-18 23:00:12 +0100

    fix : fix github pages issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from 'react-router-dom';
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -20,7 +20,8 @@ const App = () => {
           <StarsCanvas />
         </div>
       </div>
-    </BrowserRouter>
+    </Router>
+    
   );
 }
 
